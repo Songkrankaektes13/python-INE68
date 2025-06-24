79674fc76bf9757e8174284e6356c38e36ad5399
Songkrankaektes13 <songkaekteskt@gmail.com>
2025-06-24 16:50:09 +0700

    commid

--- Lecture-01/testprint.py
+++ Lecture-01/testprint.py
@@ -4,8 +4,10 @@ print("I'm so hot")
 print('he asked me"who got the book"')
 print("I'am kran \" I am so hot\"")
 print('/\\')
-'''
+
 
 print('\ta\na\ta\ta\n\ta')
-
+print ("%d"%100)
 print("my age is" + srt('25') + "i have" + srt(3500.50)+"bth.")
+'''
+print(5+4)
