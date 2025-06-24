26f78775232ddc68b52f0e7033873222f72c0cba
Songkrankaektes13 <songkaekteskt@gmail.com>
2025-06-24 16:37:13 +0700

    MyFirstCommit

---
+++ Lecture-01/testprint.py
@@ -0,0 +1,11 @@
+'''
+print('hello wold')
+print("I'm so hot")
+print('he asked me"who got the book"')
+print("I'am kran \" I am so hot\"")
+print('/\\')
+'''
+
+print('\ta\na\ta\ta\n\ta')
+
+rankaektes
