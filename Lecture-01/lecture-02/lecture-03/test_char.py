908cc7c5c06ebeaf337dac031273cdd616b60d9c
Songkrankaektes13 <songkaekteskt@gmail.com>
2025-07-08 15:05:22 +0700

    TEST.3.

--- Lecture-01/lecture-02/lecture-03/test_char.py
+++ Lecture-01/lecture-02/lecture-03/test_char.py
@@ -1,3 +1,4 @@
+'''
 inchar = input("Input your character: ")
 if inchar >= 'A' and inchar <= 'z':
     print("You in put upper Case Latter" , inchar)
@@ -7,3 +8,4 @@ elif inchar >= '0' and inchar <= '9':
     print("You in put Number Case Latter" , inchar)
 else:
     print("It's not a letter or number." , inchar)
+'''
