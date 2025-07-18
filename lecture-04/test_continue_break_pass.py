a9f0c180dee6a29c5c5b487536f2d45aa9601b1e
Songkrankaektes13 <songkrankaekteskt@gmail.com>
2025-07-18 18:32:16 +0700

    lacture-04

---
+++ lecture-04/test_continue_break_pass.py
@@ -0,0 +1,14 @@
+for letter in 'Songkran kaektes':
+    if letter == 'a' or letter == 'k':
+        continue
+    print('Current Latter:', letter)
+
+for letter in 'Songkran kaektes':
+    if letter == 'a' or letter == 'k':
+        break
+    print('Current Latter:', letter)
+
+for letter in 'Songkran kaektes':
+    if letter == 'a' or letter == 'k':
+        pass
+    print('Current Latter:', letter)
---
+++ lecture-04/test_runing_total.py
@@ -0,0 +1,8 @@
+numbers = [6,5,3,4,8,2,4,11]
+sum = 0
+
+for val in numbers:
+    sum += val
+    print(sum)
+
+print("The sum is", sum)
