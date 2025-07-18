196e78ce3949f8addeec86a0ef18562b90bf4acc
Songkrankaektes13 <songkrankaekteskt@gmail.com>
2025-07-18 20:22:59 +0700

    lectuer-04

--- lecture-04/exercise_02.py
+++ lecture-04/exercise_02.py
@@ -1,4 +1,7 @@
 rows = int(input("Enter the number of rows:"))
 columns = int(input("Enter the number of columns:"))
 
-print 
+for i in range(rows):
+    for j in range(columns):
+        print("*", end=" ")
+    print()
