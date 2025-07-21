12481d89babc22a2cbd0a92e2a4cf20a42a64e1b
Songkrankaektes13 <songkrankaekteskt@gmail.com>
2025-07-21 21:59:01 +0700

    lectur-05

---
+++ lecture-05/fucntion_parameters_03.py
@@ -0,0 +1,7 @@
+def greet(name="World"):
+    print(f"Hello, {name}!")
+
+greet()
+
+greet("Alice")
+
---
+++ lecture-05/global_variables.py
@@ -0,0 +1,8 @@
+global_variable = "I'm outside the fucntion"
+
+def my_fucntion():
+    print(global_variable)
+
+my_fucntion()
+
+print(global_variable)
