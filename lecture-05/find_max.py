cfedfc84f92f28c589d58f3d19840a4d01b5359a
Songkrankaektes13 <songkrankaekteskt@gmail.com>
2025-07-22 14:19:55 +0700

    lecture-05

--- lecture-05/find_max.py
+++ lecture-05/find_max.py
@@ -1,11 +1,17 @@
-def find_max(*args):
+def find_max(*args): # * is list
     if not args:
         return None
     max_value = args[0]
     for number in args:
         if number > max_value:
             max_value = number
-    return max_value
+    return max_value 
 
-result = find_max(3, 5, 7, 2, 8)
+result = find_max(3, 5, 7, 2, 8) # list 8 is more
+print(f"The maximum value is: {result}")
+
+result = find_max(3) # list 8 is more
+print(f"The maximum value is: {result}")
+
+result = find_max() # list 8 is more
 print(f"The maximum value is: {result}")
